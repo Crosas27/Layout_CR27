d4c01e94a519b8c0b1e9aaf72c5dc663ce619520
Crosas27 <c.rosas27@icloud.com>
2026-03-12 00:55:19 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-import { initUI } from "./js/ui/uiController.js"
+import { generateLayout } from "./js/core/layoutEngine.js"
+import { renderSvg } from "./js/renderer/svgRenderer.js"
+import { renderRibTable } from "./js/renderer/ribTableRenderer.js"
 
-document.addEventListener("DOMContentLoaded",()=>{
+export function updateLayout(){
 
-initUI()
+const config={
 
-})
+wallLength:Number(document.getElementById("wallLength").value),
+panelCoverage:Number(document.getElementById("panelCoverage").value),
+ribSpacing:Number(document.getElementById("ribSpacing").value),
+startOffset:Number(document.getElementById("startOffset").value),
+wallType:document.getElementById("wallType").value
+
+}
+
+const model=generateLayout(config)
+
+renderSvg(model)
+
+renderRibTable(model)
+
+}
